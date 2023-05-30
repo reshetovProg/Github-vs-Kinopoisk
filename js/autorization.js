69d2c7b96768501c7eb4726f092fdfcac6551cbc
reshetovProg <94145533+reshetovProg@users.noreply.github.com>
2023-05-30 16:46:58 +0300

    Add files via upload

--- js/autorization.js
+++ js/autorization.js
@@ -4,13 +4,26 @@ console.log("log");
 const container = document.querySelector('.container-autorization');
 const esc = container.querySelector('.esc');
 esc.addEventListener('click', () => {
+
 	container.style.display = 'none';
+	autoForm.style.display = 'block';
+	registerForm.style.display = 'none';
+
 })
 
-const signIn = document.querySelector('.menu__sign-in');
-console.log(signIn);
-signIn.addEventListener('click', () => {
+const signInMenu = document.querySelector('.menu__sign-in');
+signInMenu.addEventListener('click', () => {
 	console.log("push");
 	container.style.display = 'flex';
 })
 
+const signUp = document.querySelector('.signUp');
+
+const autoForm = document.querySelector('#auto');
+
+const registerForm = document.querySelector('#registration');
+signUp.addEventListener('click', () => {
+	autoForm.style.display = 'none';
+	registerForm.style.display = 'block';
+	console.log("pppp");
+})
